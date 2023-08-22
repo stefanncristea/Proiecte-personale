8555132a86170fd45968d3de781d0ffe12fb24b5
stefanncristea <79245021+stefanncristea@users.noreply.github.com>
2023-08-22 18:21:01 +0300

    Delete components directory

--- components/user-area.php
+++
@@ -1,4 +0,0 @@
-<ul>
-	<li><a href="account.php">MY ACCOUNT</a></li>
-	<li><a href="resources/logout.php">LOG OUT</a></li>
-</ul>
--- components/user-login.php
+++
@@ -1,5 +0,0 @@
-<ul>
-	<li><a href="login.php">SIGN IN</a></li>
-	<li><p>|</p></li>
-	<li><a href="register.php">REGISTER</a></li>
-</ul>
